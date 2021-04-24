4462c8a89d7dd23ab288f2f08275cd47299986ed
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-04-24 08:49:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task9_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите Z: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            Vector vector0 = new Vector(x, y, z);
+            Vector vector1 = new Vector(4, 4, 4);
+            Console.WriteLine("1 вектор " + vector0.ToString());
+            Console.WriteLine();
+            Console.WriteLine("2 вектор " + vector1.ToString());
+            Vector vector2 = vector0 + vector1;
+            Console.WriteLine();
+            Console.WriteLine("Результат сложения 1 и 2 вектора " + vector2.ToString());
+            vector2 = vector0 - vector1;
+            Console.WriteLine();
+            Console.WriteLine("Результат вычитания 1 и 2 вектора " + vector2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task9_1
+{
+    class Vector
+    {
+        private double aX = 0;
+        private double aY = 0;
+        private double aZ = 0;
+
+        private double bX;
+        private double bY;
+        private double bZ;
+
+        public Vector(double x, double y, double z)
+        {
+            bX = x;
+            bY = y;
+            bZ = z;
+        }
+
+        public double CoordinateX
+        {
+            get
+            {
+                return bX - aX;
+            }
+        }
+
+        public double CoordinateY
+        {
+            get
+            {
+                return bY - aY;
+            }
+        }
+
+        public double CoordinateZ
+        {
+            get
+            {
+                return bZ - aZ;
+            }
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(CoordinateX, 2) + Math.Pow(CoordinateY, 2) + Math.Pow(CoordinateZ, 2));
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.CoordinateX + b.CoordinateX, a.CoordinateY + b.CoordinateY, a.CoordinateZ + b.CoordinateZ);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.CoordinateX - b.CoordinateX, a.CoordinateY - b.CoordinateY, a.CoordinateZ - b.CoordinateZ);
+        }
+
+        public override string ToString()
+        {
+            return $"Длинна вектора: {Length()}" + $"\nКоординаты вектора: ({CoordinateX},{CoordinateY},{CoordinateZ})";
+        }
+    }
+}
